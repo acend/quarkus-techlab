cd3cecd4cc8dce3f32b4719fd2e51d518914f619
rhertle <hertle@puzzle.ch>
2021-09-15 18:51:02 +0200

    Update kafka lab with devservices and fix rest endpoint

--- solution/reactive-messaging/quarkus-reactive-messaging-consumer/src/main/java/ch/puzzle/consumer/boundary/DataConsumerResource.java
+++ solution/reactive-messaging/quarkus-reactive-messaging-consumer/src/main/java/ch/puzzle/consumer/boundary/DataConsumerResource.java
@@ -1,7 +1,30 @@
 package ch.puzzle.consumer.boundary;
 
+import ch.puzzle.consumer.entity.SensorMeasurement;
+import io.smallrye.mutiny.Multi;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.jboss.resteasy.reactive.RestSseElementType;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.logging.Logger;
 
 @Path("/data")
 public class DataConsumerResource {
+
+    @Inject
+    @Channel("in-memory-stream")
+    Multi<SensorMeasurement> channel;
+
+    private Logger logger = Logger.getLogger(DataConsumerResource.class.getName());
+
+    @GET
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    @RestSseElementType(MediaType.APPLICATION_JSON)
+    public Multi<SensorMeasurement> stream() {
+        return channel;
+    }
 }
--- solution/reactive-messaging/quarkus-reactive-messaging-consumer/src/main/java/ch/puzzle/consumer/boundary/ReactiveDataConsumer.java
+++ solution/reactive-messaging/quarkus-reactive-messaging-consumer/src/main/java/ch/puzzle/consumer/boundary/ReactiveDataConsumer.java
@@ -1,13 +1,11 @@
 package ch.puzzle.consumer.boundary;
 
 import ch.puzzle.consumer.entity.SensorMeasurement;
-import io.smallrye.reactive.messaging.annotations.Broadcast;
 import org.eclipse.microprofile.reactive.messaging.Acknowledgment;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.json.bind.JsonbBuilder;
 import java.util.logging.Logger;
 
 @ApplicationScoped
@@ -16,9 +14,9 @@ public class ReactiveDataConsumer {
     private static final Logger log = Logger.getLogger(ReactiveDataConsumer.class.getName());
 
     @Incoming("data-inbound")
+    @Outgoing("in-memory-stream")
     @Acknowledgment(Acknowledgment.Strategy.POST_PROCESSING)
     public SensorMeasurement consume(SensorMeasurement sensorMeasurement) {
-        log.info(JsonbBuilder.create().toJson(sensorMeasurement));
         return sensorMeasurement;
     }
 }
