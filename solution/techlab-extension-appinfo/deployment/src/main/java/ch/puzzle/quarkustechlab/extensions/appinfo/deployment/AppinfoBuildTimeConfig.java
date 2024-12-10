296de1827f748dbdf03b19b50fe347cb84e04924
christofluethi <christofluethi@users.noreply.github.com>
2024-12-10 16:17:09 +0100

    fixes for using @ConfigMapping in quarkus extensions

--- solution/techlab-extension-appinfo/deployment/src/main/java/ch/puzzle/quarkustechlab/extensions/appinfo/deployment/AppinfoBuildTimeConfig.java
+++ solution/techlab-extension-appinfo/deployment/src/main/java/ch/puzzle/quarkustechlab/extensions/appinfo/deployment/AppinfoBuildTimeConfig.java
@@ -1,35 +1,36 @@
 package ch.puzzle.quarkustechlab.extensions.appinfo.deployment;
 
 import ch.puzzle.quarkustechlab.extensions.appinfo.runtime.AppinfoNames;
-import io.quarkus.runtime.annotations.ConfigItem;
 import io.quarkus.runtime.annotations.ConfigPhase;
 import io.quarkus.runtime.annotations.ConfigRoot;
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithDefault;
 
-@ConfigRoot(name = AppinfoNames.EXTENSION_NAME, phase = ConfigPhase.BUILD_TIME)
-public class AppinfoBuildTimeConfig {
+@ConfigMapping(prefix = AppinfoNames.CONFIG_PREFIX)
+@ConfigRoot(phase = ConfigPhase.BUILD_TIME)
+public interface AppinfoBuildTimeConfig {
 
     /**
      * Simple builtFor information string
      */
-    @ConfigItem
-    String builtFor;
+    String builtFor();
 
     /**
      * Include build time collection feature in build
      */
-    @ConfigItem(defaultValue = "true")
-    boolean recordBuildTime;
+    @WithDefault("true")
+    boolean recordBuildTime();
 
     /**
      * Always include this. By default this will only be included in dev and test.
      * Setting this to true will also include this in Prod
      */
-    @ConfigItem(defaultValue = "false")
-    boolean alwaysInclude;
+    @WithDefault("false")
+    boolean alwaysInclude();
 
     /**
      * Specify basePath for extension endpoint
      */
-    @ConfigItem(defaultValue = AppinfoNames.EXTENSION_NAME)
-    String basePath;
+    @WithDefault(AppinfoNames.EXTENSION_NAME)
+    String basePath();
 }
