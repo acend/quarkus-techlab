129b8a2b065850d2bc55b4b611e1d1ef742a5107
Max Burri <burri@puzzle.ch>
2025-08-22 11:21:30 +0200

    fix: use correct syntax for RETURNING in query

--- solution/quarkus-reactive-rest-producer/src/main/java/ch/puzzle/quarkustechlab/reactiverest/producer/entity/SensorMeasurement.java
+++ solution/quarkus-reactive-rest-producer/src/main/java/ch/puzzle/quarkustechlab/reactiverest/producer/entity/SensorMeasurement.java
@@ -39,10 +39,10 @@ public class SensorMeasurement {
     }
 
     public Uni<SensorMeasurement> save(PgPool client) {
-        return client.preparedQuery("INSERT INTO sensormeasurements (data, time) VALUES ($1, $2) RETURNING (id, data, time)")
+        return client.preparedQuery("INSERT INTO sensormeasurements (data, time) VALUES ($1, $2) RETURNING id, data, time")
                 .execute(Tuple.of(data, time.atOffset(ZoneOffset.UTC)))
                 .onItem().transform(RowSet::iterator)
-                .onItem().transform(iterator -> iterator.hasNext() ? this : null);
+                .onItem().transform(iterator -> iterator.hasNext() ? new SensorMeasurement(iterator.next()) : null);
     }
 
     public static Uni<SensorMeasurement> getLatest(PgPool client) {
